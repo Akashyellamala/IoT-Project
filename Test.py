0a682b1ad4a6c4712e50fecbd3deece375f01869
Akashyellamala <167077919+Akashyellamala@users.noreply.github.com>
2024-04-26 19:23:56 +1000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,28 @@
+import os
+from paho.mqtt.client import Client, MQTTv311, CallbackAPIVersion
+
+try:
+    CLIENT_USER=os.environ['MQTT_USER']
+    CLIENT_PASS=os.environ['MQTT_PASS']
+except KeyError as e:
+    raise EnvironmentError(f'{e} not bound in environment')
+
+client = Client(
+    CallbackAPIVersion.VERSION2,
+    client_id="",
+    protocol=MQTTv311
+)
+client.username_pw_set(username=CLIENT_USER, password=CLIENT_PASS)
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe("application/4dcbfc34-d6ef-49c2-99d5-992b20d80fac/device/+/event/up")
+
+def on_message(client, userdata, msg):
+    print(f"{msg.topic}: {msg.payload!s}")
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("cqu-iot.ovangle.com", 1884)
+client.loop_forever()
